29f8b39aa4d2cdd796420202eb875565a2b5c7b6
Oksana Semonenko <ok.semonenko@gmail.com>
2016-03-25 13:44:07 +0300

    Добавляет функцию для вывода статистических сообщений getMessage.

--- build/js/check.js
+++ build/js/check.js
@@ -1,23 +1,36 @@
-var a;
-var b;
-
 function getMessage(a, b) {
-    if (typeof(a) === "boolean" && a) {
-        return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
-    } else if (typeof(a) === "boolean" && a == false) {
-        return ('Переданное GIF-изображение не анимировано');
-    } else if (typeof(a) === "number") {
-        return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
-    } else if (Object.prototype.toString.call(a) == '[object Array]') {
+    if (typeof(a) === "boolean" && a == true) {
 
-        if (Object.prototype.toString.call(b) == '[object Array]') {
-            return ('Пока!');
+        return ('Переданное GIF-изображение анимировано и содержит ' + b + ' кадров');
+
+    } else if (typeof(a) === "boolean" && a == false) {
+
+        return ('Переданное GIF-изображение не анимировано');
+
+    } else if (typeof(a) === "number") {
+
+        return ('Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов');
+
+    } else if (Array.isArray(a)) {
+
+        if (Array.isArray(b)) {
+
+            var arr = [];
+            var square = 0;
+            for (var i = 0; i < a.length; i++) {
+                arr.push(a[i] * b[i]);
+                square += arr[i];
+
+            }
+
+            return ('Общая площадь артефактов сжатия: ' + square + ' пикселей');
         }
+
         var sum = 0;
-        for(var i in a) { sum = sum + a[i]; }
+        for (var i = 0; i < a.length; i++) {
+            sum = sum + a[i];
+        }
+
         return ('Количество красных точек во всех строчках изображения: ' + sum);
-    }
-    else {
-        return false;
     }
 }
