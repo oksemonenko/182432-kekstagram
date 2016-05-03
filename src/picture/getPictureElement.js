39b03be116071824c80eb07f2a7973362723e09c
Oksana Semonenko <ok.semonenko@gmail.com>
2016-05-03 22:56:23 +0300

    Переписывает обработчик клика по картинке на записывание ее адреса в хэш вместо показа галереи.

--- src/picture/getPictureElement.js
+++ src/picture/getPictureElement.js
@@ -6,7 +6,7 @@
 'use strict';
 
 define([
-  './../gallery'
+  '../gallery'
 ], function(gallery) {
   /** @constant {number} */
   var IMAGE_LOAD_TIMEOUT = 15000;
@@ -49,7 +49,8 @@ define([
     }, IMAGE_LOAD_TIMEOUT);
     element.addEventListener('click', function(evt) {
       evt.preventDefault();
-      gallery.showGallery(pictureIndex);
+      //gallery.showGallery(pictureIndex);
+      location.hash = 'photo/' + gallery.pictureUrl(pictureIndex);
     });
     return element;
   };
